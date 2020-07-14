5c8f0175ffe6ddb46e3d89e697ffcfb5ede2faa0
groot2001 <youngboy71@163.com>
2020-07-15 00:37:26 +0800

    JsonMock封装未完成

--- src/test/java/test_service/mock/MockTest.java
+++ src/test/java/test_service/mock/MockTest.java
@@ -10,6 +10,13 @@ import net.lightbody.bmp.util.HttpMessageInfo;
 import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.*;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static common.Util.JsonMock;
+
 public class MockTest {
     private static BrowserMobProxy proxy;
 
@@ -23,11 +30,16 @@ public class MockTest {
     void before(){
         proxy.newHar("dump");
     }
+
     @Test
     void mockOnProxy() throws InterruptedException {
+        Map<String, Object> map = new HashMap<String, Object>();
         proxy.addResponseFilter(new ResponseFilter() {
             @Override
             public void filterResponse(HttpResponse httpResponse, HttpMessageContents httpMessageContents, HttpMessageInfo httpMessageInfo) {
+                System.out.println(httpMessageContents.getTextContents());
+
+                JsonMock(map, httpMessageContents.getTextContents());
                 httpMessageContents.setTextContents(httpMessageContents.getTextContents().replace("霍格沃兹", "mock"));
             }
         });
@@ -37,10 +49,6 @@ public class MockTest {
     @AfterEach
     void after(){
         Har har = proxy.endHar();
-        System.out.println(har.getLog().getPages().stream().map(h->{
-            System.out.println(h.getComment().toString());
-            return h;
-        }));
     }
 
     @AfterAll
